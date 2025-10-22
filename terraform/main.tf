0af57dc8b0fff12e8ea8e10cf2e236b99190330e
CraciunIonut <alexionutcraciun@gmail.com>
2025-10-22 22:53:46 +0300

    Modified gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,6 @@ resource "aws_lightsail_instance" "watchit-test" {
     bundle_id = "small_3_0"
 }
 
-resource "aws_lightsail_instance" "watchit-prd1" {
-    name = "watchit-test"
-    availability_zone = "eu-central-1"
-    blueprint_id = "amazon_linux_2023"
-    bundle_id = "small_3_0"
-}
-
 variable "AWS_ACCESS_KEY_ID" {
     type = string
     sensitive = true
--- utils/check-changes-plan.py
+++ utils/check-changes-plan.py
@@ -1,17 +1,31 @@
 import json
 import sys
 
+#TODO: resource_changes is a list of changes -> parse all of them and check the resource_changes.change.action for "no-op" 
+# if any action is different from "no-op" then it means that it has changes, return true else return false
+
 def main():
-    print(sys.argv)
     if len(sys.argv) != 2:
-        print("Example usage: python3 ./check-changes-plan.py <file-path>")
+        sys.stderr.write("Error: Invalid number of arguments")
         sys.exit(1)
+
     file_path = sys.argv[1]
+
+    flag = False
+
     with open(file=file_path, mode='r', encoding='utf-8') as f:
         plan_json = json.load(f)
-        print(json.dumps(plan_json['resource_changes']))
-        
 
+        for resource_change in plan_json["resource_changes"]:
+            for action in resource_change["change"]["actions"]:
+                if action != 'no-op':
+                    flag = True
+    
+    print(flag)
+
+    f.close()
+                    
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
